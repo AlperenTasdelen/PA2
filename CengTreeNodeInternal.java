21f12eed9b68ae81c74816df2b5ad0bfea9b0398
Jabloon <115788918+Jabloon@users.noreply.github.com>
2024-01-13 20:18:55 +0300

    aaaa

--- CengTreeNodeInternal.java
+++ CengTreeNodeInternal.java
@@ -131,7 +131,7 @@ public class CengTreeNodeInternal extends CengTreeNode
             System.out.println("root pushed up");
             CengTreeNodeInternal left = new CengTreeNodeInternal(this);
             CengTreeNodeInternal right = new CengTreeNodeInternal(this);
-            if(true) return;
+
             for(int i = 0; i < keyCount(); i++){
                 if(i < keyCount() / 2){
                     left.addKey(keyAtIndex(i));
@@ -157,8 +157,10 @@ public class CengTreeNodeInternal extends CengTreeNode
                     right.addChild(children.get(i));
                     children.get(i).setParent(right);
                 }
+                removeChildAtIndex(i);
+                i--;
             }
-            children.clear();
+            
             children.add(left);
             children.add(right);
 
