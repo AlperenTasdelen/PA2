9c3c2d17d4225cbbc7cb337d91b54fd5b48866e7
Jabloon <115788918+Jabloon@users.noreply.github.com>
2024-01-13 20:04:28 +0300

    aaaa

--- CengTree.java
+++ CengTree.java
@@ -143,56 +143,7 @@ public class CengTree
          */
 
         //TODO: implement tab structure
-
         print(root);
-
-        /*
-        ArrayList<CengTreeNode> queue = new ArrayList<CengTreeNode>();
-        queue.add(root);
-        while(queue.size() > 0){
-            CengTreeNode node = queue.get(0);
-            
-            if(node.getType() == CengNodeType.Internal){
-                CengTreeNodeInternal internal = (CengTreeNodeInternal) node;
-                for(int i = 0; i < queue.get(0).level; i++){
-                    System.out.print("\t");
-                }
-                System.out.println("<index>");
-                for(int i = 0; i < internal.keyCount(); i++){
-                    for(int j = 0; j < queue.get(0).level; j++){
-                        System.out.print("\t");
-                    }
-                    System.out.println(internal.keyAtIndex(i));
-                }
-                for(int i = 0; i < queue.get(0).level; i++){
-                    System.out.print("\t");
-                }
-                System.out.println("</index>");
-                for(int i = 0; i < internal.getAllChildren().size(); i++){
-                    queue.add(internal.getAllChildren().get(i));
-                }
-            }
-            else{
-                CengTreeNodeLeaf leaf = (CengTreeNodeLeaf) node;
-                for(int i = 0; i < queue.get(0).level; i++){
-                    System.out.print("\t");
-                }
-                System.out.println("<data>");
-                for(int i = 0; i < leaf.bookCount(); i++){
-                    //System.out.println("<record>" + leaf.bookAtIndex(i).getBookID() + "|" + leaf.bookAtIndex(i).getBookTitle() + "|" + leaf.bookAtIndex(i).getAuthor() + "|" + leaf.bookAtIndex(i).getGenre() + "</record>");
-                    for(int j = 0; j < queue.get(0).level; j++){
-                        System.out.print("\t");
-                    }
-                    System.out.println("<record>" + leaf.bookAtIndex(i).fullName() + "</record>");
-                }
-                for(int i = 0; i < queue.get(0).level; i++){
-                    System.out.print("\t");
-                }
-                System.out.println("</data>");
-            }
-            queue.remove(0);
-        }
-        */
     }
     // Any extra functions...
 }
